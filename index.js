c16f68c362ad246ef77d25bc3e0b8af38b728721
UDAYVEERUS <udayveerus348567@gmail.com>
2023-11-25 13:09:29 +0530

    connect to server

---
+++ index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const connectToMongo = require('./db')
+connectToMongo()
+
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+app.use("/user", require('./routes/users/route'))
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started at port ${process.env.PORT}`)
+})
